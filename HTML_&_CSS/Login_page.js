9c8c1b33578143c397671c8a57e1a00c5cf067b0
Lokesh_Patidar <bansiyalokesh29988@gmail.com>
2022-07-09 21:32:36 +0530

    added my loginpage

---
+++ HTML_&_CSS/Login_page.js
@@ -0,0 +1,11 @@
+let mail = document.getElementById("email").value;
+    //console.log(mail);
+    let password = document.getElementById("pass").value;
+    //console.log(password);
+
+    function login(){
+        alert("Successfully Loged In")
+    }
+    function logout(){
+        alert("You are Loged Out")
+    }
