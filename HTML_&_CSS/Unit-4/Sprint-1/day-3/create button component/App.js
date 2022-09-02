737b54316c17cfb39cc493361e63d2ca5ca63637
Lokesh_Patidar <bansiyalokesh29988@gmail.com>
2022-09-02 19:49:58 +0530

    CREATE A BUTTON COMPONENT

---
+++ HTML_&_CSS/Unit-4/Sprint-1/day-3/create button component/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Button from "./Components/Button";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello, Lokesh </h1>
+      <Button text="Lokesh Click me!"></Button>
+    </div>
+  );
+}
