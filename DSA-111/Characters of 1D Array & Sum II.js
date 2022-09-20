cdc75d302d23accf93cebdd0854a871afb4f595e
Lokesh_Patidar <bansiyalokesh29988@gmail.com>
2022-09-20 07:17:29 +0530

    Characters of 1D Array & Sum II

---
+++ DSA-111/Characters of 1D Array & Sum II.js
@@ -0,0 +1,36 @@
+// Description
+// You are given a string, whose size is stored in a variable with the nameN
+// The string is stored in a variable with the nameN
+// Each element of the string contains one of these symbols -'@','%','&','#','^'
+// The symbols represent the following values
+
+// '@'-> 0
+// '%'-> 1
+// '&'-> 2 
+// '#'-> 3
+// '^'-> 4
+// You have to find the sum of the string, such that for each symbol add the value of its corresponding values
+// For example, consider the value stored inN = 5, and the value stored instr = @%&#^
+// Therefore, replacing each value with its corresponding value, we get -01234
+// Now, adding all these numbers, we get0 + 1 + 2 + 3 + 4 = 10, which is the required output
+
+let N = 5;
+let str = "@%&#^";
+
+let obj={};
+let string="@%&#^";
+for(let i=0; i<string.length; i++)
+{
+    var char=string[i];
+    obj[char]=i;
+}
+
+let sum=0;
+for(let i=0; i<N; i++)
+{
+    var char=str[i];
+    sum=sum+obj[char];
+
+}
+
+console.log(sum);
