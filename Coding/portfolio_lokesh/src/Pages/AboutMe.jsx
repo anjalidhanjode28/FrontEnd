ed35b50402f9b383403fbf6743638106fd057b20
Lokesh_Patidar <bansiyalokesh29988@gmail.com>
2022-10-22 21:37:15 +0530

    my portfolio

---
+++ Coding/portfolio_lokesh/src/Pages/AboutMe.jsx
@@ -0,0 +1,10 @@
+import "./About.css";
+import React from "react";
+
+export const AboutPage = () => {
+    return (
+      <div>
+         <div></div>
+      </div>
+    );
+}
--- Coding/portfolio_lokesh/src/Pages/MainPage.jsx
+++ Coding/portfolio_lokesh/src/Pages/MainPage.jsx
@@ -1,5 +1,14 @@
 import React from "react";
-import {  FaHome, FaUser, FaIdCard, FaFolder, FaLinkedinIn,  FaGithubAlt, FaList, FaBook } from "react-icons/fa";
+import {
+  FaHome,
+  FaUser,
+  FaIdCard,
+  FaFolder,
+  FaLinkedinIn,
+  FaGithubAlt,
+  FaList,
+  FaBook,
+} from "react-icons/fa";
 import Typical from "react-typical";
 import Image from "../Image/lokesh.jpg";
 import { Link } from "@chakra-ui/react";
@@ -46,80 +55,34 @@ function MainPage() {
               </a>
             </li>
             <li>
-              <a href="https://drive.google.com/file/d/1LQC4qMoLclFHtr-zcAokIuGxgoP59AU-/view?usp=sharing" target="_blank" alt="Resume">
-                <FaBook/>
+              <a
+                href="https://drive.google.com/file/d/1LQC4qMoLclFHtr-zcAokIuGxgoP59AU-/view?usp=sharing"
+                target="_blank"
+                alt="Resume"
+              >
+                <FaBook />
                 &nbsp; Resume
               </a>
             </li>
           </ul>
         </div>
 
-        {/* right box 
+        {/* right box
          */}
         <div className="main-content">
-
           {/* Home section  */}
           <section className="home section hidden">
             <div className="container">
               <div className="profile-container">
                 <div className="profile-parent">
-                  <div className="profile-details">
-                    <div className="colz">
-                      <div className="colz-icon">
-                        <a href="https://www.linkedin.com/in/lokesh-patidar-aa45a0236/" target="_blank" alt="linked in"><i className=""><FaLinkedinIn/></i></a>
-                        <a href="https://github.com/lokesh-patidar" target="_blank" alt="github"><i className=""><FaGithubAlt/></i></a>
-                      </div>
-                    </div>
+                  
 
-                    <div className="profile-details-name">
-                      <div className="profile-details-role">
-                      <h6>Hello, I'm <span className="highlighted-text"> &nbsp; Lokesh Patidar</span></h6>
-                      <span className="primary-text">
-                        {" "}
-                        <h2>
-                          {" "}
-                          <Typical 
-                           loop={Infinity}
-                           steps={[
-                            "Enthusiastic Developer",
-                            2000,
-                            "Full Stack Developer",
-                            2000,
-                            "Mern Stack Developer",
-                            2000,
-                            "React-Redux Developer",
-                            2000,
-                           ]}
-                          />
-                        </h2>
-                      </span>
-                      <span className="profile-tag-line">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, voluptatibus?  Lorem ipsum dolor sit amet consectetur, adipisicing eli Knack of building applications with front-end and back-end operations</span>
-                      </div>
-                    </div>
-
-                    <div>
-                      <button className="btn primary-btn">
-                        {" "}
-                        Hire Me{" "}
-                      </button>
-                      <a href="https://drive.google.com/file/d/1LQC4qMoLclFHtr-zcAokIuGxgoP59AU-/view?usp=sharing" download>
-                       <button className="btn highlighted-btn">Get Resume</button>
-                      </a>
-                    </div>
-                  </div>
-                   
-                  <div className="profile-picture">
-                     <div className="profile-picture-background">
-                      <img src="https://avatars.githubusercontent.com/u/105616033?v=4" alt="lokesh-patidar"/>
-                     </div>
-                  </div>
-
+                  
                 </div>
               </div>
               <div></div>
             </div>
           </section>
-
 
           {/* About me  */}
 
@@ -134,40 +97,154 @@ function MainPage() {
                 <div className="about-content padd-15">
                   <div className="row">
                     <div className="about-text padd-15">
-                      <h3>I'm Lokesh Patidar <span>Full Stack Web Developer</span></h3>
-                      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga quas incidunt voluptatem nisi fugiat nemo consequatur eius explicabo velit tempore aut ipsam fugit harum, pariatur sequi dolorem, error repellendus dolor? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique veniam nisi excepturi in vitae laudantium quia necessitatibus inventore dicta! Explicabo vero ab nihil fugit mollitia. Aut perspiciatis eos, distinctio .</p>
+                      <h3>
+                        I'm Lokesh Patidar <span>Full Stack Web Developer</span>
+                      </h3>
+                      <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Fuga quas incidunt voluptatem nisi fugiat nemo
+                        consequatur eius explicabo velit tempore aut ipsam fugit
+                        harum, pariatur sequi dolorem, error repellendus dolor?
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Similique veniam nisi excepturi in vitae
+                        laudantium quia necessitatibus inventore dicta!
+                        Explicabo vero ab nihil fugit mollitia. Aut perspiciatis
+                        eos, distinctio .
+                      </p>
                     </div>
                   </div>
                   <div className="row">
                     <div className="personal-info padd-15">
                       <div className="row">
                         <div className="info-item">
-                          <p>Birthday : <span>14 August 1998</span></p>
+                          <p>
+                            Birthday : <span>14 August 1998</span>
+                          </p>
                         </div>
                         <div className="info-item">
-                          <p>Age : <span>24 years old</span></p>
+                          <p>
+                            Age : <span>24 years old</span>
+                          </p>
                         </div>
                         <div className="info-item">
-                          <p>Phone : <span>9993977820</span></p>
+                          <p>
+                            Phone : <span>9993977820</span>
+                          </p>
                         </div>
                         <div className="info-item">
-                          <p>Email : <span>bansiyalokesh29988@gmail.com</span></p>
+                          <p>
+                            Email : <span>bansiyalokesh29988@gmail.com</span>
+                          </p>
                         </div>
                         <div className="info-item">
-                          <p>Village : <span>Badagaon (M.P.)</span></p>
+                          <p>
+                            Village : <span>Badagaon (M.P.)</span>
+                          </p>
                         </div>
                         <div className="info-item">
-                          <p>Education : <span>12th (Secondary Education)</span></p>
+                          <p>
+                            Education : <span>12th (Secondary Education)</span>
+                            <br />
+                            <span>Full Stack-Web Developer</span>
+                          </p>
                         </div>
                       </div>
                       <div className="row">
                         <div className="buttons padd-15">
-                          <a href="#">Resume</a>
-                          <a href="#">Hire Me</a>
+                          <a
+                            href="https://drive.google.com/file/d/1LQC4qMoLclFHtr-zcAokIuGxgoP59AU-/view?usp=sharing"
+                            download
+                          >
+                            <button className="btn highlighted-btn">
+                              Get Resume
+                            </button>
+                          </a>
+                          <a href="#">
+                            <button button className="btn primary-btn">
+                              Hire Me
+                            </button>
+                          </a>
                         </div>
                       </div>
                     </div>
-                    <div className="skills padd-15">Skills</div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section className="skill section">
+            <div className="container">
+              <div className="row">
+                <div className="section-title padd-15">
+                  <h2>Skills</h2>
+                  <div className="skills">
+                    <div className="insideskills">
+
+                      <img
+                        src="https://img.shields.io/badge/chakra ui-%234ED1C5.svg?style=for-the-badge&logo=chakraui&logoColor=white"
+                        align="center"
+                        alt="chakra-ui"
+                      />
+                      <img
+                        src="https://img.shields.io/badge/rest api-%23000000.svg?style=for-the-badge&logo=flask&logoColor=white"
+                        align="center"
+                        alt="restapi"
+                      />
+                      
+                      <img
+                        src="https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB"
+                        align="center"
+                        alt="express"
+                      />
+                      <img
+                        src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white"
+                        align="center"
+                        alt="node.js"
+                      />
+                      <img
+                        src="https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white"
+                        align="center"
+                        alt="type script"
+                      />
+                      
+                      <img
+                        src="https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white"
+                        align="center"
+                        alt="mongodb"
+                      />
+                      <img
+                        src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB"
+                        align="center"
+                        alt="reactjs"
+                      />
+                      <img
+                        src="https://img.shields.io/badge/Redux-593D88?style=for-the-badge&logo=redux&logoColor=white"
+                        align="center"
+                        alt="redux"
+                      />
+                      <img
+                        src="https://img.shields.io/badge/Material%20UI-007FFF?style=for-the-badge&logo=mui&logoColor=white"
+                        align="center"
+                        alt="material-ui"
+                      />
+                      <img
+                        src="https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white"
+                        align="center"
+                        alt="react-router"
+                      />
+                    </div>
+                  </div>
+                  <div className="tools">
+                    <h4>Tools</h4>
+                    <div className="insideTools">
+                      <img src="https://img.shields.io/badge/heroku-%23430098.svg?style=for-the-badge&logo=heroku&logoColor=white" align="center" alt="git" />
+                      <img src="https://img.shields.io/badge/netlify-%23000000.svg?style=for-the-badge&logo=netlify&logoColor=#00C7B7" align="center" alt="git" />
+                      <img src="https://img.shields.io/badge/vercel-%23000000.svg?style=for-the-badge&logo=vercel&logoColor=whit" align="center" alt="git" />
+                      <img src="https://img.shields.io/badge/NPM-%23000000.svg?style=for-the-badge&logo=npm&logoColor=white" align="center" alt="npm" />
+                      <img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" align="center" alt="github" />
+                      <img src="https://img.shields.io/badge/Postman-FF6C37?style=for-the-badge&logo=postman&logoColor=white" align="center" alt="postman" />
+                    </div>
                   </div>
                 </div>
               </div>
